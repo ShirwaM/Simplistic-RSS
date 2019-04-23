8fcad6268257e43090f0f44a90a8f654bec93937
KukyMan <kukys410@gmail.com>
2019-04-23 19:15:38 +0200

    Update RssHandler.java
    Update: RSSHandler.java to not be special character sensitive.

--- library/src/com/shirwa/simplistic_rss/RssHandler.java
+++ library/src/com/shirwa/simplistic_rss/RssHandler.java
@@ -1,6 +1,8 @@
 package com.shirwa.simplistic_rss;
 
 
+import android.util.Log;
+
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
@@ -31,7 +33,8 @@ public class RssHandler extends DefaultHandler {
     private boolean parsingTitle;
     private boolean parsingLink;
     private boolean parsingDescription;
-
+    
+    private String tmpValue="";
     public RssHandler() {
         //Initializes a new ArrayList that will hold all the generated RSS items.
         rssItemList = new ArrayList<RssItem>();
@@ -45,6 +48,8 @@ public class RssHandler extends DefaultHandler {
     //Called when an opening tag is reached, such as <item> or <title>
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        tmpValue = "";
+        Log.i("Debugg","start "+qName);
         if (qName.equals("item"))
             currentItem = new RssItem();
         else if (qName.equals("title"))
@@ -52,7 +57,7 @@ public class RssHandler extends DefaultHandler {
         else if (qName.equals("link"))
             parsingLink = true;
         else if (qName.equals("description"))
-            parsingDescription = true;
+            parsingDescription=true;
         else if (qName.equals("media:thumbnail") || qName.equals("media:content") || qName.equals("image")) {
             if (attributes.getValue("url") != null)
                 currentItem.setImageUrl(attributes.getValue("url"));
@@ -62,6 +67,17 @@ public class RssHandler extends DefaultHandler {
     //Called when a closing tag is reached, such as </item> or </title>
     @Override
     public void endElement(String uri, String localName, String qName) throws SAXException {
+        if (currentItem != null) {
+            //If parsingTitle is true, then that means we are inside a <title> tag so the text is the title of an item.
+            if (parsingTitle)
+                currentItem.setTitle(tmpValue);
+                //If parsingLink is true, then that means we are inside a <link> tag so the text is the link of an item.
+            else if (parsingLink)
+                currentItem.setLink(tmpValue);
+                //If parsingDescription is true, then that means we are inside a <description> tag so the text is the description of an item.
+            else if (parsingDescription)
+                currentItem.setDescription(tmpValue);
+        }
         if (qName.equals("item")) {
             //End of an item so add the currentItem to the list of items.
             rssItemList.add(currentItem);
@@ -71,23 +87,15 @@ public class RssHandler extends DefaultHandler {
         else if (qName.equals("link"))
             parsingLink = false;
         else if (qName.equals("description"))
-            parsingDescription = false;
+            parsingDescription=false;
+
+
     }
 
     //Goes through character by character when parsing whats inside of a tag.
     @Override
     public void characters(char[] ch, int start, int length) throws SAXException {
-        if (currentItem != null) {
-            //If parsingTitle is true, then that means we are inside a <title> tag so the text is the title of an item.
-            if (parsingTitle)
-                currentItem.setTitle(new String(ch, start, length));
-                //If parsingLink is true, then that means we are inside a <link> tag so the text is the link of an item.
-            else if (parsingLink)
-                currentItem.setLink(new String(ch, start, length));
-                //If parsingDescription is true, then that means we are inside a <description> tag so the text is the description of an item.
-            else if (parsingDescription)
-                currentItem.setDescription(new String(ch, start, length));
-        }
+        tmpValue += new String(ch, start, length);
     }
 }
 
